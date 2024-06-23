13c865470f9359828f84df7c8be720fe701dfdd4
Reyhane Heidari <reyhane.heidari3640@gmail.com>
2024-06-23 16:53:01 +0330

    tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center:true
+      },
+      colors: {
+        light01:"#F9ECD5",
+        starkwhite:"#E3D5BC",
+        dark02:"#C3B297",
+        dark03:"#A49073",
+        lightgreen:"#BDE1D5",
+        shadowgreen:"#A7CDC0",
+        darkgreen:"#86AFA0",
+        darkgreen02:"#679382"
+      },
+    },
+  },
+  plugins: [],
+}
+
